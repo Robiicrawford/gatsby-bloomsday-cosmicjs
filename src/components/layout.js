ec14e23ac191da2088bbdc5084bb2cca92d9615a
Robiicrawford <50127771+Robiicrawford@users.noreply.github.com>
2022-03-20 11:20:22 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,10 +37,10 @@ export default ({ children, location }) => (
       let header
 
       let rootPath = `/`
-      let postsPath = `/posts`
+      let postsPath = `/results`
       if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
         rootPath = __PATH_PREFIX__ + `/`
-        postsPath = __PATH_PREFIX__ + `/posts`
+        postsPath = __PATH_PREFIX__ + `/results`
       }
 
       if (location.pathname === rootPath || location.pathname === postsPath) {
